3922aeb4d6da88a27a74530dd7581788740cd870
Vkiselev1984 <vkiselev-opec@mail.ru>
2023-12-20 17:08:47 +0300

    Add My App ToDoApp
    Add My App ToDoApp

---
+++ services/FileIOService.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp.services
+{
+    internal class FileIOService
+    {
+        private readonly string PATH;
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+
+        public BindingList<ToDoModel> loadDate()
+        {
+            var fileExists = File.Exists(PATH);
+           if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<ToDoModel>();
+            }
+           using (var reaaader = File.OpenText(PATH))
+            {
+                var fileText = reaaader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<ToDoModel>>(fileText);
+            }
+           
+        }
+
+        public void SaveDate(object todoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(todoDataList);
+                writer.Write(output);
+            }
+        }
+    }
+}
